984c976e0fbeab1c57a139451e82974d90dd5403
banzhe <banzhe5@outlook.com>
2025-10-31 08:28:26 +0800

    chore: Add UnoCSS integration and update dependencies

---
+++ uno.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig, presetWind3,presetAttributify,presetIcons } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetWind3(),
+    presetAttributify(),
+    presetIcons(),
+  ],
+})
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import UnoCSS from "unocss/vite";
 import solid from "vite-plugin-solid";
 
 // @ts-expect-error process is a nodejs global
@@ -6,7 +7,7 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [solid()],
+  plugins: [UnoCSS(), solid()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
